cae3f28ca69d3fbd9d50217ed97de7a107e61187
Tycho10101 <107083420+Tycho10101@users.noreply.github.com>
2024-09-14 11:00:59 -1000

    No more spam

--- bruhconverter.py
+++ bruhconverter.py
@@ -4,7 +4,6 @@ from PIL import Image
 def get_pixel_hex(x, y):
     r, g, b = img.getpixel((x, y))
     hex_value = "{:02x}{:02x}{:02x}".format(r, g, b)
-    print(hex_value)
     
     return hex_value
 
@@ -17,22 +16,17 @@ for infile in sys.argv[1:]:
                 im.save(outfile)
         except OSError:
             print("cannot convert", infile)
-            
+print("converted to bmp")
 img = Image.open(outfile)
 img = img.convert('RGB')
 
 width, height = img.size
-print(width)
-print(height)
 
 bwidth = width.to_bytes(length=4, byteorder=sys.byteorder)
 bheight = height.to_bytes(length=4, byteorder=sys.byteorder)
-print(bwidth)
-print(bheight)
 
 bruh = bwidth + bheight
-print(bruh)
-
+print("bruh-fing")
 iy = 0
 while iy < height:
     ix = 0
@@ -44,8 +38,7 @@ while iy < height:
     if not iy == height:
         bruh = bruh + "\n".encode('utf-8')
   
-print(bruh)
+print("done")
 
 with open(f + ".bruh", "wb") as bruhfile:
     bruhfile.write(bruh)
-}
